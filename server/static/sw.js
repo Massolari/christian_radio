eabe6f71513c30d5852639fecf4f9d367c427ad6
Douglas M. <douglasmassolari@hotmail.com>
2024-10-30 19:04:16 +0100

    refactor(sw): use arrow function without curly braces

--- server/static/sw.js
+++ server/static/sw.js
@@ -10,9 +10,9 @@ const ASSETS_TO_CACHE = [
 // Instalação do Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => 
+      cache.addAll(ASSETS_TO_CACHE)
+    )
   );
   // Force o novo service worker a se tornar ativo imediatamente
   self.skipWaiting();
@@ -22,27 +22,27 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     // Limpa os caches antigos
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
+    caches.keys().then((cacheNames) => 
+      Promise.all(
         cacheNames
-          .filter((cacheName) => {
+          .filter((cacheName) => 
             // Remove caches antigos (que começam com 'christian-radio-' mas não são o atual)
-            return cacheName.startsWith('christian-radio-') && cacheName !== CACHE_NAME;
-          })
-          .map((cacheName) => {
-            return caches.delete(cacheName);
-          })
-      );
-    })
+            cacheName.startsWith('christian-radio-') && cacheName !== CACHE_NAME
+          )
+          .map((cacheName) => 
+            caches.delete(cacheName)
+          )
+      )
+    )
   );
 });
 
 // Interceptação de requisições
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then((response) =>
       // Retorna do cache se encontrar, senão faz a requisição
-      return response || fetch(event.request);
-    })
+      response || fetch(event.request)
+    )
   );
 }); 
